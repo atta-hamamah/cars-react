9518495b2a149886b18a37d03b338a3c18dbcbd1
‪atta Hamamah <7atta3@gmail.com>
2024-09-18 16:57:38 +0300

    h-fix

--- src/Component/LogIn.js
+++ src/Component/LogIn.js
@@ -7,7 +7,7 @@ function LogIn() {
     const usernameRegex = /^[a-zA-Z0-9]{2,}$/
     localStorage.setItem('userName', val)
   return (
-    <div className='p-16 w-full h-screen h-100% flex flex-col justify-center  align-middle items-center text-center'>
+    <div className='p-16 w-full h-[calc(100vh-40px)] flex flex-col justify-center  align-middle items-center text-center'>
         <p className=' sm:text-5xl mb-20 text-2xl text-purple-600 font-extrabold'>Welcome to ATTA's cars dashboard</p>
         <p className='  font-semibold text-gray-600'>Enter your name to log in</p>
         <form className='mt-8'>
